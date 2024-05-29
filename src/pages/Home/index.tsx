5c6511f675b4ae7c448c6bfdcb194d54169ae41d
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-29 09:06:35 -0300

    feat: add axios and consume api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react"
+import api from "../../services/api"
+
 export default function Home() {
+
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get("movie/now_playing?", {
+        params: {
+          api_key: import.meta.env.VITE_KEY_API,
+          language: "pt-BR"
+        }
+      })
+      console.log(response.data.results)
+    }
+
+    loadMovies()
+
+  }, [])
+  
   return (
     <div>
       <h1>Bem vindo a Home</h1>
