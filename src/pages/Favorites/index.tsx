2318b96afed520ff8f1853173fb19ba510b5dc6f
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-30 16:37:47 -0300

    feat: create page favorites

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./favorites.module.css"
+
+interface Movies {
+  id: number;
+  title: string;
+  poster_path: string;
+}
+
+export default function Favorites() {
+
+  const [favoritesMovies, setFavoritesMovies] = useState<Movies[]>([])
+
+  useEffect(() => {
+    const myFavoritesList = localStorage.getItem("prime_flix_favorite")
+
+    let movies = JSON.parse(myFavoritesList) || []
+
+    setFavoritesMovies(movies)
+  },[])
+
+  function deleteFavoriteMovie(movie_id) {
+    const myFavoritesList = localStorage.getItem("prime_flix_favorite")
+
+    let myList = JSON.parse(myFavoritesList) || []
+
+    let newList = Array.isArray(myList) && myList.filter((movie) => movie.id !== movie_id)
+
+    setFavoritesMovies(newList)
+    localStorage.setItem("prime_flix_favorite", JSON.stringify(newList))
+  }
+
+  return (
+    <div className={styles.container}>
+      {favoritesMovies.length !== 0 ? <h1>Meus Favoritos</h1> : <h1>Sua lista está vazia :(</h1>}
+      <div className={styles.list_movies}>
+        {favoritesMovies.map((movie) => (
+          <article key={movie.id}>
+            <strong>{movie.title}</strong>
+            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+            <Link to={`/movie/${movie.id}`}>Acessar</Link>
+            <button onClick={() => deleteFavoriteMovie(movie.id)}>Excluir</button>
+          </article>
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Movie from './pages/Movie'
 import Header from './components/Header'
 import Erro from './pages/Erro'
+import Favorites from './pages/Favorites'
 
 export default function RoutesApp() {
   return (
@@ -11,6 +12,7 @@ export default function RoutesApp() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/movie/:id' element={<Movie/>} />
+        <Route path='/favorites' element={<Favorites/>} />
         <Route path='*' element={<Erro/>} />
       </Routes>
     </BrowserRouter>
